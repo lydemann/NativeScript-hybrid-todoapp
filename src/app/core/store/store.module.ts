e115f75c46c1e138d6d3cd05312f3fd52bfff629
Christian Ludemann <ext.christian.lude@ef.com>
2019-03-06 07:39:36 +0100

    feat: migrated to shared nativescript project

--- src/app/core/store/store.module.ts
+++ src/app/core/store/store.module.ts
@@ -3,7 +3,7 @@ import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from 'environments/environment';
+import { environment } from '~/environments/environment';
 
 @NgModule({
   imports: [
